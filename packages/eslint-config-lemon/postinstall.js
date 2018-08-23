4f9e11d14c039e26f231032b93e6beb4bc1f50ac
GloriousYellow <yellowglorious@gmail.com>
2018-08-23 12:57:40 -0400

    f

--- packages/eslint-config-lemon/postinstall.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra')
-const path = require('path')
-
-if (fs.existsSync(path.resolve(__dirname, '../../node_modules'))) {
-  fs.copy(
-    path.resolve(__dirname, 'node_modules'),
-    path.resolve(__dirname, '../../node_modules'),
-  )
-}
